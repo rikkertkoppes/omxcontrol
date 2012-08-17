e85132598e65bed09f4aea368afd9be8caed226c
rikkertkoppes <rikkert@rikkertkoppes.com>
2012-08-17 22:56:00 +0200

    removed test url

--- index.js
+++ index.js
@@ -35,8 +35,6 @@ omx.express = function(req,res,next) {
     next();
 };
 
-var file = 'http://o-o---preferred---ams03s15---v19---lscache2.c.youtube.com/videoplayback?ip=86.89.231.15&upn=Fwirm5Yaelk&sparams=algorithm%2Cburst%2Ccp%2Cfactor%2Cgcr%2Cid%2Cip%2Cipbits%2Citag%2Csource%2Cupn%2Cexpire&fexp=908920%2C914057%2C904822%2C907217%2C919804%2C920704%2C912806%2C906831%2C911406%2C913550%2C912706&mt=1345230372&key=yt1&algorithm=throttle-factor&burst=40&ipbits=8&itag=22&sver=3&signature=2593ACB710B923807F0194EB927C96381630815B.11F6B8F7A1419296378EEE54C0FD3CB677882512&mv=m&source=youtube&ms=au&gcr=nl&expire=1345255588&factor=1.25&cp=U0hTSlJQU19NUkNOM19KSldGOng4aG1CaVAyblB3&id=7bf983b89baf6632';
-
 omx.start = function(fn) {
     if (!pipe) {
         pipe = 'omxcontrol';
@@ -48,7 +46,6 @@ omx.start = function(fn) {
 
     function cb(fn) {
         console.log(fn);
-        fn = file;
         exec('omxplayer -o hdmi "'+fn+'" < '+pipe,function(error, stdout, stderr) {
             console.log(stdout);
         });
