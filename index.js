333d809ecbda7009d229ea8499aa4c6934192fcc
Dragos Plesca <dragos.plesca@gmail.com>
2013-12-29 14:11:19 +0200

    remove express part

--- index.js
+++ index.js
@@ -5,46 +5,12 @@ var pipe = false;
 var map = false;
 var DEFAULT_PATH = '/omx';
 
-function omx(mapper) {
-    map = mapper;
-    return omx.express;
-}
-
-omx.express = function(req,res,next) {
-    if (req.path.indexOf(DEFAULT_PATH) === 0) {
-        //replace + and decode
-        path = decodeURIComponent(req.path.replace(/\+/g, ' '));
-        //remove leading and trailing /
-        path = path.replace(/^\/|\/$/g,'');
-        //split and remove leading path
-        var parts = path.split('/');
-        parts.shift();
-        var command = parts.shift();
-        console.log('executing',command,parts);
-        if (omx[command]) {
-            if (command === 'start') {
-                omx.start(parts.join('/')+'?'+parseurl.parse(req.url).query);
-            } else {
-                omx[command].apply(this,parts);
-            }
-            //prevent anything else from being served from this subpath
-            res.end('executed '+command);
-            return;
-        }
-    }
-    next();
-};
-
 omx.start = function(fn) {
     if (!pipe) {
         pipe = 'omxcontrol';
         exec('mkfifo '+pipe);
     }
-    if (map) {
-        map(fn,cb);
-    } else {
-        cb(fn);
-    }
+    cb(fn);
 
     function cb(fn) {
         console.log(fn);
