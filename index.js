8a57563e2616bdc9a7cbd0d1471c30e2fb2f1bbb
Rikkert Koppes <rikkert@rikkertkoppes.com>
2024-04-09 14:04:58 +0200

    formatting

--- index.js
+++ index.js
@@ -1,72 +1,77 @@
-var exec = require('child_process').exec;
-var parseurl = require('url');
+var exec = require("child_process").exec;
+var parseurl = require("url");
 
 var pipe = false;
 var map = false;
-var DEFAULT_PATH = '/omx';
+var DEFAULT_PATH = "/omx";
 
-var BINARY = 'omxplayer';
+var BINARY = "omxplayer";
 
 function omx(mapper) {
     map = mapper;
     return omx.express;
 }
 
-omx.express = function(req,res,next) {
+omx.express = function (req, res, next) {
     if (req.path.indexOf(DEFAULT_PATH) === 0) {
         //replace + and decode
-        path = decodeURIComponent(req.path.replace(/\+/g, ' '));
+        path = decodeURIComponent(req.path.replace(/\+/g, " "));
         //remove leading and trailing /
-        path = path.replace(/^\/|\/$/g,'');
+        path = path.replace(/^\/|\/$/g, "");
         //split and remove leading path
-        var parts = path.split('/');
+        var parts = path.split("/");
         parts.shift();
         var command = parts.shift();
-        console.log('executing',command,parts);
+        console.log("executing", command, parts);
         if (omx[command]) {
-            if (command === 'start') {
-                omx.start(parts.join('/')+'?'+parseurl.parse(req.url).query);
+            if (command === "start") {
+                omx.start(
+                    parts.join("/") + "?" + parseurl.parse(req.url).query
+                );
             } else {
-                omx[command].apply(this,parts);
+                omx[command].apply(this, parts);
             }
             //prevent anything else from being served from this subpath
-            res.end('executed '+command);
+            res.end("executed " + command);
             return;
         }
     }
     next();
 };
 
-omx.start = function(fn, onFinish) {
+omx.start = function (fn, onFinish) {
     if (!pipe) {
-        pipe = 'omxcontrol';
-        exec('mkfifo '+pipe);
+        pipe = "omxcontrol";
+        exec("mkfifo " + pipe);
     }
     if (map) {
-        map(fn,cb);
+        map(fn, cb);
     } else {
         cb(fn);
     }
 
     function cb(fn) {
         console.log(fn);
-        exec(BINARY + ' -o hdmi "'+fn+'" < '+pipe,function(error, stdout, stderr) {
-            console.log(stdout);
-            if (onFinish) {
-                onFinish();
+        exec(
+            BINARY + ' -o hdmi "' + fn + '" < ' + pipe,
+            function (error, stdout, stderr) {
+                console.log(stdout);
+                if (onFinish) {
+                    onFinish();
+                }
             }
-        });
-        exec('echo . > '+pipe);
+        );
+        exec("echo . > " + pipe);
     }
 };
 
-omx.sendKey = function(key) {
+omx.sendKey = function (key) {
     if (!pipe) return;
-    exec('echo -n '+key+' > '+pipe);
+    exec("echo -n " + key + " > " + pipe);
 };
 
-omx.mapKey = function(command,key,then) {
-    omx[command] = function() {
+omx.mapKey = function (command, key, then) {
+    omx[command] = function () {
         omx.sendKey(key);
         if (then) {
             then();
@@ -74,13 +79,13 @@ omx.mapKey = function(command,key,then) {
     };
 };
 
-omx.mapKey('pause','p');
-omx.mapKey('quit','q',function() {
-    exec('rm '+pipe);
+omx.mapKey("pause", "p");
+omx.mapKey("quit", "q", function () {
+    exec("rm " + pipe);
     pipe = false;
 });
-omx.mapKey('play','.');
-omx.mapKey('forward',"$'\\x1b\\x5b\\x43'");
-omx.mapKey('backward',"$'\\x1b\\x5b\\x44'");
+omx.mapKey("play", ".");
+omx.mapKey("forward", "$'\\x1b\\x5b\\x43'");
+omx.mapKey("backward", "$'\\x1b\\x5b\\x44'");
 
 module.exports = omx;
